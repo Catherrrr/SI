f2bb07083844dfb1284fe5c53f04e8025e83c297
Catherrrr <vitorgodoy61@gmail.com>
2024-11-06 21:31:44 -0300

    aa

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Página sobre Animes</title>
+</head>
+<body>
+    <header>
+        <h1>Manipulação de dados</h1>
+    </header>
+    <main>
+        <section id="intro">
+            <h3>A manipulação de dados é uma das principais competências no desenvolvimento de aplicações web, permitindo o armazenamento, gerenciamento e exibição de informações de maneira eficiente e dinâmica. Neste projeto, exploraremos como as linguagens PHP e SQL trabalham em conjunto para criar sistemas robustos e funcionais. O PHP, uma linguagem de script do lado do servidor, é amplamente utilizado para a construção de sites e aplicações que interagem com bancos de dados, enquanto o SQL (Structured Query Language) é a linguagem padrão para gerenciar e manipular esses dados de forma estruturada.</h3>
+            <h4>A integração dessas tecnologias possibilita operações como a criação, leitura, atualização e exclusão de dados (CRUD), essenciais em sistemas como e-commerces, plataformas de gerenciamento de clientes e diversos outros tipos de aplicações. Este trabalho acadêmico visa aprofundar o entendimento dos conceitos e práticas envolvidas na manipulação de dados, destacando a importância da segurança, boas práticas de codificação e a eficiência na execução de consultas.</h4>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+        </section>
+    
+        <section id="outro">
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+        </section>
+    </main>
+    <footer>
+        <a href="php/main.php">Manipulação de Clientes</a>
+    </footer>
+    <footer>
+        <a href="php/culturas/maincult.php">Manipulação de Culturas</a>
+    </footer>
+    <footer>
+        <a href="php/equipamentos/maineq.php">Manipulação de Equipamentos</a>
+    </footer>
+    <footer>
+        <a href="php/propriedades/mainprop.php">Manipulação de Propriedades</a>
+    </footer>
+</body>
+</html>
