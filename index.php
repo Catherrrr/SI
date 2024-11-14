26d4fa8875fbe552559a1ac1fd6ea460951d90d8
Catherrrr <vitorgodoy61@gmail.com>
2024-11-13 21:06:23 -0300

    final

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-
+''
     <h1>Minha loja de jogos</h1>
     <h2> Explore os melhores jogos de computador em um só lugar. Aqui você encontra uma seleção incrível de jogos para todos os estilos: ação, aventura, estratégia e muito mais! Com conteúdo atualizado, lançamentos e clássicos, nosso objetivo é trazer diversão e qualidade para todos os jogadores.</h2>
     <h2> Descubra o Novo e o Clássico em um Só Lugar Nossos jogos são cuidadosamente selecionados para garantir qualidade e diversão. Fique por dentro dos lançamentos mais recentes, explore favoritos de todos os tempos e aproveite recomendações personalizadas de acordo com suas preferências.</h2>
@@ -13,7 +13,7 @@
         <h1>Menu de operações</h1>
         <button id="cadastroBtn">Cadastro </button><br/><br/>
         <button id="vendaBtn">Venda</button>
-        <button id="cadastroBtn">Leandro </button><br/><br/>
+        <button id="cadastroBtn">Leandrãooo </button><br/><br/>
     </div>
     <script type="text/javascript" src="js/menu.js"></script>
 </body>
